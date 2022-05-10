f07579198a1a03f2c5ee774ea7c07605c741ce32
Itzel Gonzalez <marvin@hitchhikersguidetothegalaxy.com>
2022-05-10 15:58:30 -0500

    Se modifico para cambiar arg de entrada quaternion a Euler(RPY)

--- catkin_ws/src/manipulation/inverse_kinematics/scripts/traj_srv.py
+++ catkin_ws/src/manipulation/inverse_kinematics/scripts/traj_srv.py
@@ -15,10 +15,10 @@ from manip_msgs.srv import *
 
 from std_srvs.srv import Empty, EmptyResponse
 
+tm = 0.05
 
-
+# Calcula polinomio 5o orden para una variable
 def calcula_tray(tt, pi, pf, vi, vf, ai, af):
-    tm = 0.05   
     print("tiempo de muestreo tm: ",tm)
     for p in range(0,len(tt)-1):
         # Rango de tiempo en el que se evalua el polinomio
@@ -48,6 +48,7 @@ def calcula_tray(tt, pi, pf, vi, vf, ai, af):
     return [t, xpos, xvel, xacel]
 
 
+tm = 0.05 # tiempo de muestreo
 
 def grafica_tray(t, x, y, z, lbl, a,b,c,y_label):
     plot(t,x,t,y,t,z)
@@ -62,10 +63,15 @@ def grafica_tray(t, x, y, z, lbl, a,b,c,y_label):
 
     
 def cartesian_traj(tt, pi, pf, vi, vf, ai, af):
-
+    """
+    Genera trayectorias para x,y,z,R,P a partir de condiciones iniciales y finales, 
+    empaqueta cada punto en un mensaje JointTrajectoryPoint dentro de una lista en
+    un objeto JointTrajectory y retorna este objeto
+    """
+    
     p6, v6, a6, t = np.empty(0), np.empty(0), np.empty(0), np.empty(0)
-    # Posiciones ocurren en el orden x,y,z,R,P, Orientacion: Row, Pitch, Yaw rango (-180, 180)
-    for i in range(6):  # Generar las trayectorias en el espacio cartesiano
+    # Posiciones ocurren en el orden x,y,z,R,P, Orientacion: Row, Pitch, Yaw 
+    for i in range(6):  # Genera las trayectorias en el espacio cartesiano
         t, p, v, a = calcula_tray(tt, pi[i], pf[i], vi[i], vf[i], ai[i], af[i])
         p6 ,v6,a6 = np.append(p6, [p]), np.append(v6, [v]), np.append(a6, [a])
 
@@ -74,12 +80,15 @@ def cartesian_traj(tt, pi, pf, vi, vf, ai, af):
     traj = JointTrajectory()    #trayectoria con los puntos en 3d
     traj.joint_names = ["x","y","z","row","pitch","yaw"]
     i = 0
+    tfs = 0
     for i in range(len(p)):    # Para cada punto de la trayectoria
-        point = JointTrajectoryPoint()  # Vreamos un objeto que almacena los datos de 1 punto
-        point.positions = p6[0,i], p6[1,i],p6[2,i], np.deg2rad(p6[3,i]),np.deg2rad(p6[4,i]), np.deg2rad(p6[5,i])
+        point = JointTrajectoryPoint()  # Creamos un objeto que almacena los datos de 1 punto
+        point.positions = p6[0,i], p6[1,i],p6[2,i], (p6[3,i]), (p6[4,i]), (p6[5,i])
         #point.velocities = 0,0,0,0,0,0#v6[0,i],v6[1,i],v6[2,i],v6[3,i],v6[4,i],v6[5,i]
         #point.accelerations = 0,0,0,0,0,0#a6[0,i],a6[1,i],a6[2,i],a6[3,i],a6[4,i],a6[5,i]
-        traj.points.append(point)
+        point.time_from_start.secs = tfs
+        #print("time from start", point.time_from_start.secs)
+        tfs += tm
 
     grafica_tray(t, p6[0,:], p6[1,:], p6[2,:],'Posicion en X,Y,Z','X', 'Y','Z','Distancia[m]')
     grafica_tray(t, p6[3,:], p6[4,:], p6[5,:],'Orientacion Row, Pitch, Yaw','R','P','Y','Distancia[grados]')
@@ -88,13 +97,17 @@ def cartesian_traj(tt, pi, pf, vi, vf, ai, af):
 
 
 def callback_trajectory(req):  #request es de tipo Pose
-    # Pide tiempo de trayectoria y condiciones inicialea de pos,vel y acel 
+    # req = Pose p1, p2 (orientacion en cuaterniones) , float64 t
+    angles1 = tft.euler_from_quaternion([req.p1.orientation.x , req.p1.orientation.y ,req.p1.orientation.z, req.p1.orientation.w])
+    angles2 = tft.euler_from_quaternion([req.p2.orientation.x , req.p2.orientation.y ,req.p2.orientation.z, req.p2.orientation.w])
+    
+    ang1, ang2 = list(angles1), list(angles2)
     t = req.t 
     tt = np.array([0,t])
     # pi = [0.15, 0.7, 0.4, 0.0, 0.0, 0.0]
-    pi = [req.p1.positions.x, req.p1.positions.y, req.p1.positions.z, 0.0, 0.0, 0.0]
+    pi = [req.p1.positions.x, req.p1.positions.y, req.p1.positions.z, ang1[0], ang1[1], ang1[2]]
     #pf = [0.35, 0.4, 0.5, 90, 0.0, 0.0]
-    pf = [req.p2.positions.x, req.p2.positions.y, req.p2.positions.z, 90, 0.0, 0.0]
+    pf = [req.p2.positions.x, req.p2.positions.y, req.p2.positions.z, ang2[0], ang2[1], ang2[2]]
 
     vi = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
     vf = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
@@ -102,7 +115,8 @@ def callback_trajectory(req):  #request es de tipo Pose
     af = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
 
     trajectory = cartesian_traj(tt, pi, pf, vi, vf, ai, af)
-    #print(cartesian_traj(tt, pi, pf, vi, vf, ai, af))
+    
+    print(cartesian_traj(tt, pi, pf, vi, vf, ai, af))
     print("Se llamo al servicio genera trayectoria espacio cartesiano")
     return trajectory    # Retorna un objeto JointTrajectory
 
