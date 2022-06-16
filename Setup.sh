a273d5139e7251da9cf5333b5fa83549f551cd9d
Itzel <biddygonzalez@gmail.com>
2022-06-16 13:38:53 -0500

    CHANGES TO MAKE IT WORK IN UBUNTU 20

--- Setup.sh
+++ Setup.sh
@@ -1,1 +1,23 @@
-sudo apt install ros-melodic-graph-msgs 
+sudo apt-get install ros-noetic-serial
+sudo apt install ros-melodic-graph-msgs
+sudo apt install ros-noetic-dynamixel-sdk
+sudo apt install ros-noetic-random-numbers
+sudo apt install ros-noetic-tf2-bullet -y
+sudo apt install ros-noetic-map-server -y
+sudo apt install ros-noetic-amcl -y
+sudo apt install ros-noetic-move-base -y
+sudo apt install ros-noetic-random-numbers -y
+sudo apt install ros-noetic-moveit-kinematics -y
+sudo apt install ros-noetic-sound-play -y
+sudo apt install ros-noetic-dwa-local-planner
+sudo apt install ros-noetic-gazebo-dev 
+sudo apt install ros-noetic-gazebo-msgs 
+sudo apt install ros-noetic-gazebo-plugins
+sudo apt install ros-noetic-gazebo-ros
+sudo apt install ros-noetic-gazebo-ros-control
+sudo apt install ros-noetic-gazebo-ros-pkgs
+sudo apt install ros-noetic-ros-control ros-noetic-ros-controllers ros-noetic-controller-manager ros-noetic-controller-interface ros-noetic-effort-controllers ros-noetic-joint-state-controller ros-noetic-joint-state-publisher
+sudo apt install portaudio19-dev
+sudo apt install kolourpaint4
+sudo pip install pyaudio
+sudo pip install pocketsphinx
--- catkin_ws/src/hardware/gazebo_envs/scripts/topic_resender.py
+++ catkin_ws/src/hardware/gazebo_envs/scripts/topic_resender.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import rospy
 from std_msgs.msg import Float32MultiArray, Float32, Float64
 from sensor_msgs.msg import JointState
